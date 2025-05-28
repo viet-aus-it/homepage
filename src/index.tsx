dcad3e3cb0c40807f4087ea2f52a626714c66925
Sam Huynh <sam.huynh@lyka.com.au>
2025-05-29 00:27:57 +1000

    chore: remove msw error

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './app';
 
 async function renderRoot() {
-  if (import.meta.env.DEV) {
-    const { worker } = await import('../__mocks__/msw/browser');
-    await worker.start();
-  }
-
   const RootComponent = (
     <StrictMode>
       <App />
