30bd6e736452dfea6e6769099a68fb276fffe7be
Sam Huynh <sam.huynh@lyka.com.au>
2025-05-28 23:51:03 +1000

    infra: add cloudfront and s3 deployment

--- infra/lib/homepage-stack.ts
+++ infra/lib/homepage-stack.ts
@@ -1,8 +1,58 @@
+import path from 'node:path';
 import * as cdk from 'aws-cdk-lib';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as cloudfront_origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3_deployment from 'aws-cdk-lib/aws-s3-deployment';
 import type { Construct } from 'constructs';
 
 export class HomepageStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const siteBucket = new s3.Bucket(this, 'HomepageSiteBucket', {
+      bucketName: 'vait-homepage-content-bucket',
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+    new cdk.CfnOutput(this, 'BucketArn', { value: siteBucket.bucketArn });
+
+    const distribution = new cloudfront.Distribution(this, 'HomepageSiteDistribution', {
+      defaultRootObject: 'index.html',
+      defaultBehavior: {
+        origin: cloudfront_origins.S3BucketOrigin.withOriginAccessControl(siteBucket),
+        compress: true,
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      errorResponses: [
+        {
+          httpStatus: 403,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+        },
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+        },
+      ],
+      comment: 'Homepage Cloudfront Distribution',
+    });
+    distribution.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
+    new cdk.CfnOutput(this, 'DistributionId', { value: distribution.distributionId });
+
+    const clientPath = path.resolve(__dirname, '..', '..', 'dist');
+    const clientBundle = s3_deployment.Source.asset(clientPath);
+
+    new s3_deployment.BucketDeployment(this, 'DeployWithInvalidation', {
+      sources: [clientBundle],
+      destinationBucket: siteBucket,
+      distribution,
+      distributionPaths: ['/*'],
+    });
+    new cdk.CfnOutput(this, 'DistributionDomain', { value: distribution.distributionDomainName });
   }
 }
