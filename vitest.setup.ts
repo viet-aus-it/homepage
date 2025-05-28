c01e3cbd3c46e4b1f3e3294631db05e76c5ae84c
Sam Huynh <sam.huynh@lyka.com.au>
2025-05-28 23:17:43 +1000

    chore: remove msw because its unlikely we'll need it

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,7 +1,6 @@
 import '@testing-library/jest-dom/vitest';
 import { cleanup } from '@testing-library/react';
 import { afterEach } from 'vitest';
-import './__mocks__/msw/setup';
 
 afterEach(() => {
   cleanup();
