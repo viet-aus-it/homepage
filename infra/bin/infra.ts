2619022b0e4aaedbabea20f459f66e56f26b8b80
Sam Huynh <sam.huynh@lyka.com.au>
2025-05-30 09:19:46 +1000

    infra: add custom domain

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -3,4 +3,9 @@ import * as cdk from 'aws-cdk-lib';
 import { HomepageStack } from '../lib/homepage-stack';
 
 const app = new cdk.App();
-new HomepageStack(app, 'HomepageStack', {});
+new HomepageStack(app, 'HomepageStack', {
+  env: {
+    account: process.env.AWS_ACCOUNT_ID,
+    region: 'ap-southeast-2',
+  },
+});
--- infra/lib/homepage-stack.ts
+++ infra/lib/homepage-stack.ts
@@ -1,17 +1,32 @@
 import path from 'node:path';
 import * as cdk from 'aws-cdk-lib';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as cloudfront_origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as route53_targets from 'aws-cdk-lib/aws-route53-targets';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3_deployment from 'aws-cdk-lib/aws-s3-deployment';
 import type { Construct } from 'constructs';
+
+const BASE_DOMAIN = 'vietausit.com';
 
 export class HomepageStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const zone = route53.HostedZone.fromLookup(this, 'VAITHostedZone', { domainName: BASE_DOMAIN });
+    const siteDomain = `home.${BASE_DOMAIN}`;
+    new cdk.CfnOutput(this, 'SiteDomain', { value: `https://${siteDomain}` });
+
+    const certificate = new acm.Certificate(this, 'SiteCertificate', {
+      domainName: siteDomain,
+      validation: acm.CertificateValidation.fromDns(zone),
+    });
+    new cdk.CfnOutput(this, 'Certificate', { value: certificate.certificateArn });
+
     const siteBucket = new s3.Bucket(this, 'HomepageSiteBucket', {
-      bucketName: 'vait-homepage-content-bucket',
+      bucketName: siteDomain,
       publicReadAccess: false,
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
@@ -21,6 +36,8 @@ export class HomepageStack extends cdk.Stack {
 
     const distribution = new cloudfront.Distribution(this, 'HomepageSiteDistribution', {
       defaultRootObject: 'index.html',
+      domainNames: [siteDomain],
+      minimumProtocolVersion: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021,
       defaultBehavior: {
         origin: cloudfront_origins.S3BucketOrigin.withOriginAccessControl(siteBucket),
         compress: true,
@@ -43,6 +60,13 @@ export class HomepageStack extends cdk.Stack {
     });
     distribution.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
     new cdk.CfnOutput(this, 'DistributionId', { value: distribution.distributionId });
+
+    const siteDomainTarget = new route53_targets.CloudFrontTarget(distribution);
+    new route53.ARecord(this, 'SiteAliasRecord', {
+      recordName: siteDomain,
+      target: route53.RecordTarget.fromAlias(siteDomainTarget),
+      zone,
+    });
 
     const clientPath = path.resolve(__dirname, '..', '..', 'dist');
     const clientBundle = s3_deployment.Source.asset(clientPath);
