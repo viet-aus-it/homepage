df536248c3365bf2ac27684014a26cb8f8fcf672
Sam Huynh <samhwang2112.dev@gmail.com>
2025-05-30 20:37:08 +1000

    feat: split certificates into a new stack in us-east-1 (#4)

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,11 +1,28 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
+
+import { CertStack } from '../lib/cert-stack';
 import { HomepageStack } from '../lib/homepage-stack';
 
 const app = new cdk.App();
-new HomepageStack(app, 'HomepageStack', {
+
+const tags = cdk.Tags.of(app);
+tags.add('app', 'Homepage');
+tags.add('repo', 'https://github.com/viet-aus-it/homepage');
+
+const certStack = new CertStack(app, 'HomepageCertStack', {
+  env: {
+    account: process.env.AWS_ACCOUNT_ID,
+    region: 'us-east-1',
+  },
+  description: 'VAIT Homepage Certification Stack',
+});
+
+const homepageStack = new HomepageStack(app, 'HomepageStack', {
   env: {
     account: process.env.AWS_ACCOUNT_ID,
     region: 'ap-southeast-2',
   },
+  description: 'VAIT Homepage Stack',
 });
+homepageStack.addDependency(certStack);
