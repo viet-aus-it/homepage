3c98eb727d1de8caecbfecb3ec9bae42950909df
Sam Huynh <sam.huynh@lyka.com.au>
2025-06-03 21:41:48 +1000

    chore: import correct tanstack router devtool component
    This has been updated as of https://tanstack.com/router/latest/docs/framework/react/devtools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,14 +1,6 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router';
-import { lazy } from 'react';
+import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 import NotFoundPage from '../pages/not-found';
-
-const TanStackRouterDevtools = import.meta.env.PROD
-  ? () => null
-  : lazy(() =>
-      import('@tanstack/router-devtools').then((res) => ({
-        default: res.TanStackRouterDevtools,
-      }))
-    );
 
 export const Route = createRootRoute({
   component: () => (
