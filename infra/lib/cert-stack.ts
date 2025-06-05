57069da45167da4efd5a7a47d47d86bf35a5604d
Sam Huynh <sam.huynh@lyka.com.au>
2025-06-05 22:19:19 +1000

    feat(infra): register cert for home and www domain

--- infra/lib/cert-stack.ts
+++ infra/lib/cert-stack.ts
@@ -3,17 +3,17 @@ import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 import * as route53 from 'aws-cdk-lib/aws-route53';
 import type { Construct } from 'constructs';
 
-import { BASE_DOMAIN } from './constants';
+import { BASE_DOMAIN, SITE_DOMAIN, WWW_DOMAIN } from './constants';
 
 export class CertStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const hostedZone = route53.HostedZone.fromLookup(this, 'VAITHostedZone', { domainName: BASE_DOMAIN });
-    const siteDomain = `home.${BASE_DOMAIN}`;
 
     const certificate = new acm.Certificate(this, 'SiteCertificate', {
-      domainName: siteDomain,
+      domainName: BASE_DOMAIN,
+      subjectAlternativeNames: [SITE_DOMAIN, WWW_DOMAIN],
       validation: acm.CertificateValidation.fromDns(hostedZone),
     });
     new cdk.CfnOutput(this, 'Certificate', { value: certificate.certificateArn });
--- infra/lib/constants.ts
+++ infra/lib/constants.ts
@@ -1,2 +1,3 @@
 export const BASE_DOMAIN = 'vietausit.com';
-export const SITE_DOMAIN = 'home.vietausit.com';
+export const SITE_DOMAIN = `home.${BASE_DOMAIN}`;
+export const WWW_DOMAIN = `www.${BASE_DOMAIN}`;
