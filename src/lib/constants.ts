aacba77490b562243620a477d72d47d800641449
Luan Nguyen <2020169+tklarryonline@users.noreply.github.com>
2025-06-10 13:37:56 +1000

    chore(constants): Correct Association Number
    myCAV incorrectly generated our registration number for incorporated association due to their faulty system. They have since created a new number for us.
    
    The new registration number for Vietnamese Australians in Information Technology Inc. is A0127262Z.

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,7 @@ export const ORGANISATION = {
   NAME: 'Vietnamese Australians in Information Technology Inc.',
   SHORT_NAME: 'VAIT',
   ABN: '54 638 315 913',
-  ASSOCIATION_NUMBER: 'A0000009Z',
+  ASSOCIATION_NUMBER: 'A0127262Z',
   ADDRESS: 'Melbourne, VIC Australia',
   DISCORD_URL: 'https://chat.vietausit.com/',
 } as const;
