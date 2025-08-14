0abe08bdb2d60da1c6311a0a613719de88f6b976
Sam Huynh <samhwang2112.dev@gmail.com>
2025-08-14 11:06:19 +1000

    chore: upgrade dependencies and remove react OXC plugin (#25)

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
 import path from 'node:path';
 import tailwindcss from '@tailwindcss/vite';
 import { tanstackRouter } from '@tanstack/router-vite-plugin';
-import react from '@vitejs/plugin-react-oxc';
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
