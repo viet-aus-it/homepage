085c042ea0bc494f0a1de8d828d70a40d6954cc5
Sam Huynh <samhwang2112.dev@gmail.com>
2026-03-24 12:48:04 +1100

    chore: update deps

--- src/components/ui/navigation-menu.tsx
+++ src/components/ui/navigation-menu.tsx
@@ -114,12 +114,12 @@ function NavigationMenuIndicator({ className, ...props }: React.ComponentProps<t
 
 export {
   NavigationMenu,
-  NavigationMenuList,
-  NavigationMenuItem,
   NavigationMenuContent,
-  NavigationMenuTrigger,
-  NavigationMenuLink,
   NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
   NavigationMenuViewport,
   navigationMenuTriggerStyle,
 };
