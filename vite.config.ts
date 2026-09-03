61dd2517d217d36d29e3d4de39796bc4cec8e5de
Sam Huynh <samhwang2112.dev@gmail.com>
2026-09-03 14:20:12 +1000

    build: opt-in to use native react compiler
    Purely for faster compilation times, less reliant on babel. Fully supported workflow by the Vite team.

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,16 @@
 import path from 'node:path';
 
 import { cloudflare } from '@cloudflare/vite-plugin';
-import babel from '@rolldown/plugin-babel';
 import tailwindcss from '@tailwindcss/vite';
 import { tanstackRouter } from '@tanstack/router-vite-plugin';
-import react, { reactCompilerPreset } from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    babel({
-      presets: [reactCompilerPreset()],
+    react({
+      compiler: true,
     }),
     tailwindcss(),
     tanstackRouter({
