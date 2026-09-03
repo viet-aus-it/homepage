fbb3d1cc3a4d111d10aaa550e2b70b0e02d4536c
Sam Huynh <samhwang2112.dev@gmail.com>
2026-09-03 14:43:01 +1000

    build: opt-in to native React compiler and bump deps (#79)
    * build: opt-in to use native react compiler
    
    Purely for faster compilation times, less reliant on babel. Fully supported workflow by the Vite team.
    
    * build: opt in to auto code splitting for chunking builds
    
    * build: fix dirname warning in build

--- vite.config.ts
+++ vite.config.ts
@@ -3,28 +3,28 @@
 import path from 'node:path';
 
 import { cloudflare } from '@cloudflare/vite-plugin';
-import babel from '@rolldown/plugin-babel';
 import tailwindcss from '@tailwindcss/vite';
 import { tanstackRouter } from '@tanstack/router-vite-plugin';
-import react, { reactCompilerPreset } from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    babel({
-      presets: [reactCompilerPreset()],
-    }),
-    tailwindcss(),
     tanstackRouter({
       routeFileIgnorePattern: '.*\\.test\\.tsx$',
+      autoCodeSplitting: true,
+      target: 'react',
     }),
+    react({
+      compiler: true,
+    }),
+    tailwindcss(),
     cloudflare(),
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(import.meta.dirname, './src'),
     },
   },
 });
